966c77b3b27f2be593fb5927a357acb82c03dfe9
JangHyukJin <wkdgurwls00@naver.com>
2018-09-20 16:38:55 +0900

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by JangHyukJin on 20/09/2018.
+//  Copyright © 2018 JangHyukJin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
