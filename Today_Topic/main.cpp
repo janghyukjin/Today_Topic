1bcf6513ac17e4f314e426e7d1e4b33ae2724e48
JangHyukJin <wkdgurwls00@naver.com>
2018-10-05 13:48:41 +0900

    curlpp 추가

--- Today_Topic/main.cpp
+++ Today_Topic/main.cpp
@@ -1,41 +1,31 @@
-//
-//  main.cpp
-//  test
-//
-//  Created by JangHyukJin on 20/09/2018.
-//  Copyright © 2018 JangHyukJin. All rights reserved.
-//
+#include <fstream>
 #include <iostream>
+#include <string>
+#include "json/json.h"
+#include "curlpp/include/curlpp/cURLpp.hpp"
+#include "curlpp/include/curlpp/Options.hpp"
+#include "curlpp/include/curlpp/Easy.hpp"
 
 using namespace std;
 
-class FruitSeller
-{
-    private:
-        int apple_price;
-        int numOfApples;
-        int myMoney;
-    public:
-        FruitSeller(int price, int num, int money)
-        {
-            apple_price= price;
-            numOfApples= num;
-            myMoney= money;
-            
-        }
-    int SaleApples(int money)
+int main(){
+    string text ="{\"title\":\"스포츠한국:레드벨벳 예리, 눈에 띄는 인형 미모(2018 아시아송페스티벌)\"}";
+    Json::Value root;
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse( text, root );
+    if ( !parsingSuccessful )
     {
-        int num = money/ apple_price;
-        numOfApples-= num;
-        myMoney+= money;
-        return num;
-        
+        cout << "Error parsing the string" << endl;
     }
-    void ShowSalesResult() const
-    {
-        cout << "the number of apples: " << numOfApples << endl;
-        cout << "the margin: " << myMoney << endl;
-        
-    }
+    cout << root.get("title","UTF-8").asString() << endl;
     
-};
+    curlpp::Cleanup myCleanup;
+    
+    // Send request and get a result.
+    // Here I use a shortcut to get it in a string stream ...
+    
+    std::ostringstream os;
+    os << curlpp::options::Url(std::string("http://www.wikipedia.org"));
+    
+    string asAskedInQuestion = os.str();
+}
